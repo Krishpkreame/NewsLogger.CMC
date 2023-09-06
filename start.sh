b6005ebaa6b16bc3ec51e49b7ab2a4fcae502d65
Krish Patel <krishpkreame1405+github@gmail.com>
2023-09-06 14:17:35 +1200

    backup

--- start.sh
+++ start.sh
@@ -5,3 +5,19 @@ pip install -r requirements.txt
 sleep 1
 clear
 python3 main.py
+
+
+# #! /bin/bash
+# echo Stated
+# while true
+# do
+#     if [ $(date +%H) -eq 01 ]
+#     then
+#         echo Running Script Now
+#         ./cmcnews/NewsLogger.CMC/start.sh
+#         break
+#     else
+#         echo Still on dw...
+#         sleep 20m
+#     fi
+# done
